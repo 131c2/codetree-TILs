481cbb61592e532cd64c139aa5c8f6ad262b2b8e
131c2 <bg3189@naver.com>
2024-09-05 08:06:34 +0900

    Title: 코로나 메뉴얼, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240905/코로나 메뉴얼/covid-manual.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tem1, tem2, tem3;
+        char symp1, symp2,symp3;
+
+        symp1 = sc.next().charAt(0);
+        tem1 = sc.nextInt();
+        symp2 = sc.next().charAt(0);
+        tem2 = sc.nextInt();
+        symp3 = sc.next().charAt(0);
+        tem3 = sc.nextInt();
+        
+
+        if(symp1 == 'Y' && tem1 >= 37){
+            if(symp2 == 'Y' && tem2 >= 37 ||symp3 == 'Y' && tem3 >= 37 ){
+                System.out.println("E");
+            }
+            else{
+                System.out.println("N");
+            }
+        }
+    }
+}
