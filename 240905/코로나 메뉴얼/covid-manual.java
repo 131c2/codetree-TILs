3ae3ff57936e935a0346d885bed6c53435ff2ac0
131c2 <bg3189@naver.com>
2024-09-05 08:19:03 +0900

    Title: 코로나 메뉴얼, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240905/코로나 메뉴얼/covid-manual.java
+++ 240905/코로나 메뉴얼/covid-manual.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int tem1, tem2, tem3;
-        char symp1, symp2,symp3;
+        char symp1, symp2, symp3;
 
         symp1 = sc.next().charAt(0);
         tem1 = sc.nextInt();
@@ -16,7 +16,7 @@ public class Main {
         
 
         if(symp1 == 'Y' && tem1 >= 37){
-            if(symp2 == 'Y' && tem2 >= 37 ||symp3 == 'Y' && tem3 >= 37 ){
+            if((symp2 == 'Y' && tem2 >= 37)||(symp3 == 'Y' && tem3 >= 37)){
                 System.out.println("E");
             }
             else{
@@ -24,7 +24,7 @@ public class Main {
             }
         }
         else{
-            if(symp2 == 'Y' && tem2 >= 37 && symp3 == 'Y' && tem3 >= 37 ){
+            if((symp2 == 'Y' && tem2 >= 37)&&(symp3 == 'Y' && tem3 >= 37)){
                 System.out.println("E");
             }
             else{
