d11fe028b0ed73cc67273d1b5a03782511edeb2d
131c2 <bg3189@naver.com>
2024-09-07 12:11:07 +0900

    Title: 감소시키며 출력하기 2, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240907/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int i;
+
+        i = 26;
+
+        while(i >= 10){
+            System.out.print(i + " ");
+            i--;
+        }
+
+    }
+}
