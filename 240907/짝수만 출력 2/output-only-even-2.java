4b7fa0a0d101c95f01c0645b502f47d42d951a77
131c2 <bg3189@naver.com>
2024-09-07 12:14:14 +0900

    Title: 짝수만 출력 2, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240907/짝수만 출력 2/output-only-even-2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, i;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        i = a;
+
+        while(i >= b){
+            System.out.print(i + " ");
+            i -= 2;
+        }
+    }
+}
