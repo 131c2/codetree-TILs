82db0172d8f32eed18dae66761438b61f84a1bd0
131c2 <bg3189@naver.com>
2024-09-16 19:01:20 +0900

    Title: 합성수 판별기, Time: 111ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240916/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n, i;
+
+        boolean satisfied = false;
+
+        n = sc.nextInt();
+
+        for(i = 2 ; i < n ; i ++){
+            if(n % i == 0){
+                satisfied = true;
+            }
+        }
+        if(satisfied = true){
+            System.out.print("C");
+        }
+        else{
+            System.out.print("N");
+        }
+    }
+}
