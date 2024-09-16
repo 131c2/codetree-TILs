822a09f138e82f40958f66ac8989857031a05d58
131c2 <bg3189@naver.com>
2024-09-16 20:25:01 +0900

    Title: 공약수의 유무, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240916/공약수의 유무/presence-or-absence-of-a-common-divisor.java
+++ 240916/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -4,12 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a, b, i, j;
+        int a, b, i;
+        //int j;
 
         boolean satisfied = false;
 
         a = sc.nextInt();
         b = sc.nextInt();
+
+        /* for문 두 개 사용
 
         for(i = 1 ; i <= 2880 ; i ++){
             if(2880 % i == 0){
@@ -23,6 +26,15 @@ public class Main {
                 }
             }            
         }
+        */
+
+        for(i = a ; i <= b ; i++){
+            if(1920 % i == 0 && 2880 % i == 0){
+                satisfied = true;
+                break;
+            }
+        }
+
         if(satisfied == true){
             System.out.print(1);
         }
