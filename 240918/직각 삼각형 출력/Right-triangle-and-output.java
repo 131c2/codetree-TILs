5db859142117a768615df0ab9b3c2476905ecc13
131c2 <bg3189@naver.com>
2024-09-18 21:49:36 +0900

    Title: 직각 삼각형 출력, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240918/직각 삼각형 출력/Right-triangle-and-output.java
+++ 240918/직각 삼각형 출력/Right-triangle-and-output.java
@@ -8,6 +8,8 @@ public class Main {
 
         n = sc.nextInt();
 
+        //내 풀이 : 별이 하나씩 증가하는 직각삼각형에 두 번째 줄부터 시작하는 직각삼각형을 하나더 올리는 느낌으로 함
+
         for(i = 1 ; i <= n ; i ++){
             for(j = 1 ; j <= i ; j ++){
                 System.out.print("*");
@@ -19,5 +21,15 @@ public class Main {
                 }
             System.out.println();
         }
+
+        /* 해설 풀이 : i 값을 0에에서 시작하여 j 값에 2를 곱한뒤 1을 더해 반복수를 맞춤.
+
+        for(i = 0 ; i < n ; i ++){
+            for(j = 0 ; j < 2 * i + 1 ; j ++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        */
     }
 }
