95bd03791b7a5155e6b6e5f07555834420e3da8e
131c2 <bg3189@naver.com>
2024-09-20 21:44:15 +0900

    Title: 두 개의 직각삼각형, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240920/두 개의 직각삼각형/two-right-triangle.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i, j, n;
+
+        n = sc.nextInt();
+
+        for(i = n ; i >= 1 ; i --){
+            for(j = 1 ; j <= i ; j ++){
+                System.out.print("*");
+            }
+            for(j = n ; j > i ; j --){
+                System.out.print("  ");
+            }
+            for(j = 1 ; j <= i ; j ++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
