425a25729448c160a90bfcca5d934257f8faf219
131c2 <bg3189@naver.com>
2024-09-21 01:02:06 +0900

    Title: 별표 출력하기 6, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240921/별표 출력하기 6/print-star-6.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i, j, n;
+
+        n = sc.nextInt();
+
+        for(i = n ; i >= 1 ; i --){
+            for(j = n ; j > i ; j --){
+                System.out.print("  ");
+            }            
+            for(j = 1 ; j <= i * 2 - 1 ; j ++){
+                System.out.print("* ");
+            }            
+            System.out.println();
+        }
+
+        for(i = 2 ; i <= n ; i ++){
+            for(j = n ; j > i ; j --){
+                System.out.print("  ");
+            }
+            for(j = 1 ; j <= i * 2 - 1; j ++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
