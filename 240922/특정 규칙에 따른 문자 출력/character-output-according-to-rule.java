bcdb2879198c92f25b4589b70dd2a7c72f3cbffe
131c2 <bg3189@naver.com>
2024-09-22 16:25:23 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240922/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i, j, n;
+
+        n = sc.nextInt();
+
+        for(i = 1 ; i <= n ; i ++){
+            for(j = i ; j < n ; j ++){
+                System.out.print("  ");
+            }
+            for(j = 1 ; j <= i ; j ++){
+                System.out.print("@ ");
+            }
+            System.out.println();
+        }
+        for(i = n ; i > 1 ; i --){
+            for(j = 1 ; j < i ; j ++){
+                System.out.print("@ ");
+            }
+            System.out.println();
+        }
+    }
+}
