afb20e7282e0f949a70de819b16f374f20755700
131c2 <bg3189@naver.com>
2024-10-07 15:35:11 +0900

    Title: 소수 구하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 241007/소수 구하기/get-prime.java
+++ 241007/소수 구하기/get-prime.java
@@ -19,5 +19,18 @@ public class Main {
                 System.out.print(i + " ");
             }
         }
+
+        /* 해설풀이
+        for(int i = 1; i <= n; i++) {
+            if(i == 1) continue;
+            boolean isprime = true;
+            
+            for(int j = 2; j < i; j++)
+                if(i % j == 0) isprime = false;
+            
+            if(isprime)
+                System.out.print(i + " ");
+        }
+        */
     }
 }
