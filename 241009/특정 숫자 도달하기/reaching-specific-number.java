ba499456945486697ec55cd82913f9cd8168b16c
131c2 <bg3189@naver.com>
2024-10-09 16:21:27 +0900

    Title: 특정 숫자 도달하기, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 241009/특정 숫자 도달하기/reaching-specific-number.java
+++ 241009/특정 숫자 도달하기/reaching-specific-number.java
@@ -12,17 +12,17 @@ public class Main {
         sum = 0;
         ave = 0;
 
-        for(i = 1 ; i <= 10 ; i ++){
+        for(i = 0 ; i < 10 ; i ++){
             a[i] = sc.nextInt();
             if(a[i] < 250){
                 sum += a[i];
             }
             else{
-                ave = (double)sum / (i-1);
+                ave = (double)sum / i;
                 break;
             }
-            if(i == 10){
-                ave = (double)sum / i;
+            if(i == 9){
+                ave = (double)sum / (i + 1);
             }
         }
         System.out.printf("%d %.1f",sum , ave);
