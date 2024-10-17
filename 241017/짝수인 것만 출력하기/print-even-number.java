df979233c4684877ac2093a5c79144c235443a2c
131c2 <bg3189@naver.com>
2024-10-17 17:36:26 +0900

    Title: 짝수인 것만 출력하기, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241017/짝수인 것만 출력하기/print-even-number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i, n;
+
+        n = sc.nextInt();
+
+        int[] a = new int[n];
+
+        for(i = 0 ; i < n ; i ++){
+            a[i] = sc.nextInt();
+            if(a[i] % 2 == 0){
+                System.out.print(a[i]);
+            }
+        }
+    }
+}
