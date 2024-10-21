6ea3a9a93c94f317a417356f0be9fd7a8adcc97f
131c2 <bg3189@naver.com>
2024-10-21 16:32:07 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241021/연속부분수열일까/contiguous-array-or-not.java
+++ 241021/연속부분수열일까/contiguous-array-or-not.java
@@ -7,6 +7,9 @@ public class Main {
         //선언
         int n1, n2, i, j, cnt;
 
+        //cnt 초기화
+        cnt = 0;
+        
         //n1, n2 입력
         n1 = sc.nextInt();
         n2 = sc.nextInt();
@@ -25,8 +28,6 @@ public class Main {
 
         //배열 비교.
         for(i = 0 ; i < n1 ; i ++){
-            //cnt 초기화
-            cnt = 0;
             if(a[i] == b[0]){
                 for(j = 0 ; j < n2 ; j ++){
                     if(a[i + j] == b[j]){
@@ -36,15 +37,21 @@ public class Main {
 
                 //cnt 값이 n2 값과 일치하면 Yes 출력 및 탈출
                 if(cnt == n2){
-                    System.out.print("Yes");
                     break;
+                }
+                // cnt값과 n2 값이 일치하지 않으면 cnt를 0으로 초기화 후 for문 진행
+                else{
+                    cnt = 0;
                 }
             }
         }
-
+        
         //for문이 끝나고 cnt 값이 0인경우 배열이 일치한 것이 없었기 때문에 No 출력
         if(cnt == 0){
             System.out.print("No");
+        }
+        else{
+            System.out.print("Yes");
         }
 
     }
