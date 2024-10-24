b361d98364fe3726dc864e21449d5e2ecba037a5
131c2 <bg3189@naver.com>
2024-10-24 19:34:18 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 235ms, Memory: 13MB, Status: Passed - CodeTree

--- 241024/중복되지 않는 숫자 중 최대/max-of-unique-number.java
+++ 241024/중복되지 않는 숫자 중 최대/max-of-unique-number.java
@@ -11,26 +11,47 @@ public class Main {
         max = INT_MIN;
 
         int[] a = new int[N];
+        int[] b = new int[N];
 
+        //b배열 초기화
+        for(i = 0 ; i < N ; i ++){
+            b[i] = 0;
+        }
+
+        //a배열 입력
         for(i = 0 ; i < N ; i ++){
             a[i] = sc.nextInt();
         }
+
+        //중복 숫자가 나왔을 경우 b배열에 1표시
         for(i = 0 ; i < N ; i ++){
             for(j = 0 ; j < N ; j ++){
                 if(a[i] == a[j] && i != j){
-                    a[i] = 0;
-                    a[j] = 0;
+                    b[i] = 1;
+                    b[j] = 1;
                 }
             }
         }
+
+        //표시된 b배열과 같은 항에 있는 a배열 0으로 만들기
+        for(i = 0 ; i < N ; i ++){
+            if(b[i] == 1){
+                a[i] = 0;
+            }
+        }
+        
+        //a배열에서 중복되지 않은 수 중 최댓값 찾기
         for(i = 0 ; i < N ; i ++){
             if(a[i] > max){
                 max = a[i];
             }
         }
+
+        //a배열의 모든 항이 0이라면 전부 중복수이기 때문에 -1출력
         if(max == 0){
             System.out.print(-1);
         }
+        //아닐경우 최댓값 max출력
         else{
             System.out.print(max);
         }
