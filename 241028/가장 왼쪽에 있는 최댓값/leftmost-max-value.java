72a05f16b6037a90a56c4a372a00630908f0014e
131c2 <bg3189@naver.com>
2024-10-28 21:33:49 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 263ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 241028/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N, i, j, idx, max, range;
+
+        N = sc.nextInt();
+        int[] a = new int[N];
+
+        range = N;
+
+        final int INT_MIN = Integer.MIN_VALUE;
+        
+        for(i = 0 ; i < N ; i ++){
+            a[i] = sc.nextInt();
+        }
+
+        for(i = 0 ; i < N ; i ++){
+            max = INT_MIN;
+            idx = -1;
+
+            for(j = 0 ; j < range ; j ++){
+                if(a[j] > max){
+                    max = a[j];
+                    idx = j + 1;
+                }
+            }
+            range = idx - 1;
+            System.out.print(idx + " ");
+            if(idx == 1){
+                break;
+            }
+        }
+    }
+}
