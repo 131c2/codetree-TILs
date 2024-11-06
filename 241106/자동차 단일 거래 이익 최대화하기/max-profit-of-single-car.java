95e7cdceda6884be9fb3775412d23798ea50b052
131c2 <bg3189@naver.com>
2024-11-06 20:58:11 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241106/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
+++ 241106/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -4,21 +4,49 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int i, j, n, max, cnt;
+        int i, j, n, max, min, cnt, idxMin, idxMax;
         final int INT_MIN = Integer.MIN_VALUE;
+        final int INT_MAX = Integer.MAX_VALUE;
 
         cnt = 0;
+        idxMin = -1;
+        idxMax = -1;
         n = sc.nextInt();
 
         max = INT_MIN;
-
+        min = INT_MAX;
+        
         int[] a = new int[n + 1];
-
-        a[n] = 0;
 
         for(i = 0 ; i < n ; i ++){
             a[i] = sc.nextInt();
         }
+
+        //최솟값의 위치 찾기
+        for(i = 0 ; i < n ; i ++){
+            if(a[i] < min){
+                min = a[i];
+                idxMin = i;
+            }
+        }
+
+        //최솟값의 뒤에 위치하면서 최댓값이 되는 항 찾기
+        for(i = 0 ; i < n ; i ++){
+            if(a[i] > max && i > idxMin){
+                max = a[i];
+            }
+        }
+        if((max - min) > 0){
+            System.out.print(max - min);
+        }
+        else{
+            System.out.print(0);
+        }
+        
+        
+
+
+        /* 잘못 이해한 풀이
         for(i = 1 ; i <= n ; i ++){
             cnt ++;
             if(a[i - 1] > a[i]){
@@ -36,6 +64,6 @@ public class Main {
         else {
             System.out.print(0);
         }
-        
+        */
     }
 }
