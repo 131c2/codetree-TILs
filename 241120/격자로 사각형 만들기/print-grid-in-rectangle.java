0c5b0e7d5a2a9e18818465a88224615e3e4e79c2
131c2 <bg3189@naver.com>
2024-11-20 14:14:30 +0900

    Title: 격자로 사각형 만들기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241120/격자로 사각형 만들기/print-grid-in-rectangle.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i, j, n;
+
+        n = sc.nextInt();
+
+        int[][] a = new int[n][n];
+
+        for(i = 0 ; i < n ; i ++){
+            a[i][0] = 1;
+            a[0][i] = 1;
+        }
+
+        for(i = 1 ; i < n ; i ++){
+            for(j = 1 ; j < n ; j ++){
+                a[i][j] = a[i - 1][j - 1] + a[i - 1][j] + a[i][j - 1];
+            }
+        }
+
+        for(i = 0 ; i < n ; i ++){
+            for(j = 0 ; j < n ; j ++){
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
