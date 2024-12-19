85b5f89a80f666bae9b6471e6ef3fa59b9e007fe
131c2 <bg3189@naver.com>
2024-12-19 21:29:27 +0900

    Title: 단어로 구분하기, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241219/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] sarr = new String[10];
+
+        int i;
+
+        for(i = 0 ; i < 10 ; i ++){
+            sarr[i] = sc.next();
+        }
+        for(i = 0 ; i < 10 ; i ++){
+            System.out.println(sarr[i]);
+        }
+    }
+}
