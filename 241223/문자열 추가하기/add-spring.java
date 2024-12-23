cb73870aefedee6292ea64634852e2ad88b0fc4a
131c2 <bg3189@naver.com>
2024-12-23 17:47:26 +0900

    Title: 문자열 추가하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241223/문자열 추가하기/add-spring.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str, str2;
+        str = sc.next();
+        str2 = "Hello";
+
+        System.out.print(str + str2);
+    }
+}
