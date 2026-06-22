42ea27a7d70aa14a784bb827b803331a8a872250
131c2 <bg3189@naver.com>
2026-06-23 00:06:13 +0900

    [Passed] Date to Date (133ms, 10MB)

---
+++ trail2/Date to Date/date-to-date.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+
+        int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        for(int i = 1 ; i < m1 ; i ++){
+            d1 += days[i];
+        }
+
+        for(int i = 1 ; i < m2 ; i ++){
+            d2 += days[i];
+        }
+
+        System.out.println(d2 - d1 + 1);
+
+
+        // while(true){
+        //     if(m1 == m2 && d1 == d2){
+        //         cnt ++;
+        //         break;
+        //     }
+
+        //     cnt ++;
+        //     d1 ++;
+
+        //     if(d1 > days[m1]){
+        //         m1 ++;
+        //         d1 = 1;
+        //     }
+        // }
+
+        // System.out.println(cnt);
+
+    }
+}
